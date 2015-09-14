866282f7f9fc0f25d370040741f0b6051dbcb568
Vicary Archangel <vicrry@yahoo.com.hk>
2015-09-14 12:04:12 +0800

    Daemonize the process

--- app.js
+++ app.js
@@ -1,32 +1,77 @@
 /*! app.js | Start the faye server. */
 
-var http = require('http')
-  , faye = require('faye')
+// note: daemonize
+if ( process.argv.indexOf('--cron') > -1 ) {
+  var spawn = require('child_process').spawn
+    , argv = process.argv;
 
-var server = http.createServer()
-  , bayeux = new faye.NodeAdapter({ mount: '/', timeout: 60 })
+  // remove --cron option
+  argv.splice(argv.indexOf('--cron'), 1);
 
-bayeux.attach(server);
-server.listen(81, function() {
-  var address = server.address();
-
-  console.info('Bayeux server listening on %s port %d.', address.address, address.port);
-});
-
-server.on('error', function(err) {
-  switch ( err.code ) {
-    // silently discard known errors
-    case 'EADDRINUSE':
-      break;
-
-    default:
-      console.error(err);
-      break;
-  }
-});
-
-process.on('SIGINT', function() {
-  server.close(function() {
-    console.info('\rBayeux server is closed.');
+  var child = spawn(argv.shift(), argv, {
+    stdio: ['ignore', 'ignore', 'ignore', 'ipc'],
+    detached: true
   });
-});
+
+  var tmId = setTimeout(function() {
+    console.error('Timeout while starting server, killing process.');
+
+    child.kill();
+  }, 30000);
+
+  child.on('message', function(message) {
+    if ( message.close ) {
+      this.disconnect();
+    }
+
+    if ( message.error ) {
+      console.error('Cannot start bayeux server: %s', message.error);
+    }
+
+    if ( tmId ) {
+      clearTimeout(tmId);
+    }
+
+    child.unref();
+  });
+}
+else {
+  var http = require('http')
+    , faye = require('faye')
+
+  var server = http.createServer()
+    , bayeux = new faye.NodeAdapter({ mount: '/', timeout: 60 })
+
+  bayeux.attach(server);
+  server.listen(81, function() {
+    var address = server.address();
+
+    console.info('Bayeux server listening on %s port %d.', address.address, address.port);
+
+    process.send({ close: true });
+  });
+
+  server.on('error', function(err) {
+    switch ( err.code ) {
+      // silently discard known errors
+      case 'EADDRINUSE':
+        process.send({ close: true });
+        break;
+
+      case 'EACCES':
+        process.send({ close: true, error: 'Permission denied to designated address.' });
+        break;
+
+      default:
+        err.close = true;
+        process.send(err);
+        break;
+    }
+  });
+
+  process.on('SIGINT', function() {
+    server.close(function() {
+      console.info('\rBayeux server is closed.');
+    });
+  });
+}
